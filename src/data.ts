6b33f12eda98c31d945fc017655c3052c32f3654
romanusovich1 <romanusovich1@gmail.com>
2023-07-01 16:02:46 +0300

    feat: add data.ts with in-memory db

---
+++ src/data.ts
@@ -0,0 +1,10 @@
+import { User } from "./types";
+
+export const Users: User[] = [
+    {
+        id: 'a6aeb63d-5629-43c3-bb10-330fba0cfebc',
+        username: 'Roman',
+        age: 22,
+        hobbies: ['Technologies', 'Nature'],
+    },
+];
