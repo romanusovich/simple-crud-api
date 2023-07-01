a32db3702cb81e4eb2002fd58023b66dc9eb15ad
romanusovich1 <romanusovich1@gmail.com>
2023-07-01 16:22:17 +0300

    refactor: remove unnecessary import

--- src/app.ts
+++ src/app.ts
@@ -2,66 +2,75 @@ import * as http from 'http';
 import 'dotenv/config';
 import { Controller } from './controller';
 import { getReqData } from './utils';
-import { resolve, reject } from './types';
+import { resolve } from './types';
 
 const PORT = process.env.STATUS === 'prod' ? process.env.PROD_PORT : process.env.DEV_PORT;
 const CONTROLLER = new Controller();
 
 const server = http.createServer(async (req, res) => {
-    // GET /api/users
-    if (req.url === '/api/users' && req.method === 'GET') {
-        const response = await CONTROLLER.getUsers() as resolve;
-        res.writeHead(response.status, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(response.data));
-    }
-    // GET /api/users/:id
-    else if (req.url?.startsWith('/api/users') && req.method === 'GET') {
-        try {
-            const id = req.url.split('/')[3];
-            const response = await CONTROLLER.getUserById(id) as resolve;
+    try {
+        // GET /api/users
+        if (req.url === '/api/users' && req.method === 'GET') {
+            const response = await CONTROLLER.getUsers() as resolve;
             res.writeHead(response.status, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(response.data));
-        } catch (e: any) {
-            res.writeHead(e.status, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(e.message));
         }
-    }
-    // POST /api/users
-    else if (req.url?.startsWith('/api/users') && req.method === 'POST') {
-        try {
-            const reqData = await getReqData(req) as string;
-            const response = await CONTROLLER.createUser(JSON.parse(reqData)) as resolve;
-            res.writeHead(response.status, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(response.data));
-        } catch (e: any) {
-            res.writeHead(e.status, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(e.message));
+        // GET /api/users/:id
+        else if (req.url?.startsWith('/api/users') && req.method === 'GET') {
+            try {
+                const id = req.url.split('/')[3];
+                const response = await CONTROLLER.getUserById(id) as resolve;
+                res.writeHead(response.status, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(response.data));
+            } catch (e: any) {
+                res.writeHead(e.status, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(e.message));
+            }
         }
-    }
-    // PUT /api/users/:id
-    else if (req.url?.startsWith('/api/users') && req.method === 'PUT') {
-        try {
-            const id = req.url.split('/')[3];
-            const reqData = await getReqData(req) as string;
-            const response = await CONTROLLER.updateUserById(id, JSON.parse(reqData)) as resolve;
-            res.writeHead(response.status, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(response.data));
-        } catch (e: any) {
-            res.writeHead(e.status, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(e.message));
+        // POST /api/users
+        else if (req.url?.startsWith('/api/users') && req.method === 'POST') {
+            try {
+                const reqData = await getReqData(req) as string;
+                const response = await CONTROLLER.createUser(JSON.parse(reqData)) as resolve;
+                res.writeHead(response.status, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(response.data));
+            } catch (e: any) {
+                res.writeHead(e.status, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(e.message));
+            }
         }
-    }
-    // DELETE /api/users/:id
-    else if (req.url?.startsWith('/api/users') && req.method === 'DELETE') {
-        try {
-            const id = req.url.split('/')[3];
-            const response = await CONTROLLER.deleteUserById(id) as resolve;
-            res.writeHead(response.status, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(response.data));
-        } catch (e: any) {
-            res.writeHead(e.status, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(e.message));
+        // PUT /api/users/:id
+        else if (req.url?.startsWith('/api/users') && req.method === 'PUT') {
+            try {
+                const id = req.url.split('/')[3];
+                const reqData = await getReqData(req) as string;
+                const response = await CONTROLLER.updateUserById(id, JSON.parse(reqData)) as resolve;
+                res.writeHead(response.status, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(response.data));
+            } catch (e: any) {
+                res.writeHead(e.status, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(e.message));
+            }
         }
+        // DELETE /api/users/:id
+        else if (req.url?.startsWith('/api/users') && req.method === 'DELETE') {
+            try {
+                const id = req.url.split('/')[3];
+                const response = await CONTROLLER.deleteUserById(id) as resolve;
+                res.writeHead(response.status, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(response.data));
+            } catch (e: any) {
+                res.writeHead(e.status, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(e.message));
+            }
+        }
+        else {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end('Route not found');
+        }
+    } catch (e) {
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end('The server is bad, please try again or try again later');
     }
 });
 
