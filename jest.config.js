f67518aabd8a2aa882748cb2e711e582b8768022
romanusovich1 <romanusovich1@gmail.com>
2023-07-03 13:51:28 +0300

    chore: add jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  testPathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/?(*.)+(test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
 };
